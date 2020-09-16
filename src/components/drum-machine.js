1b26ced4db6057f9f6d4ca80571a22fcad087c08
Maximilian Coghlan <max.coghlan@icloud.com>
2020-09-16 16:56:23 +0100

    keyboard functionality

--- src/components/drum-machine.js
+++ src/components/drum-machine.js
@@ -2,14 +2,130 @@ import React, { useState } from "react";
 import Drumpad from "./drum-pad";
 
 const DrumMachine = (props) => {
-  // STATE
-  const keyboard = ["Q", "W", "E", "A", "S", "D", "Z", "X", "C"];
+  const heaterKit = [
+    {
+      keyCode: 81,
+      keyTrigger: "Q",
+      id: "Heater-1",
+      url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3",
+    },
+    {
+      keyCode: 87,
+      keyTrigger: "W",
+      id: "Heater-2",
+      url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3",
+    },
+    {
+      keyCode: 69,
+      keyTrigger: "E",
+      id: "Heater-3",
+      url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3",
+    },
+    {
+      keyCode: 65,
+      keyTrigger: "A",
+      id: "Heater-4",
+      url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3",
+    },
+    {
+      keyCode: 83,
+      keyTrigger: "S",
+      id: "Clap",
+      url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3",
+    },
+    {
+      keyCode: 68,
+      keyTrigger: "D",
+      id: "Open-HH",
+      url: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3",
+    },
+    {
+      keyCode: 90,
+      keyTrigger: "Z",
+      id: "Kick-n'-Hat",
+      url: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3",
+    },
+    {
+      keyCode: 88,
+      keyTrigger: "X",
+      id: "Kick",
+      url: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3",
+    },
+    {
+      keyCode: 67,
+      keyTrigger: "C",
+      id: "Closed-HH",
+      url: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3",
+    },
+  ];
+
+  const bankTwo = [
+    {
+      keyCode: 81,
+      keyTrigger: "Q",
+      id: "Chord-1",
+      url: "https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3",
+    },
+    {
+      keyCode: 87,
+      keyTrigger: "W",
+      id: "Chord-2",
+      url: "https://s3.amazonaws.com/freecodecamp/drums/Chord_2.mp3",
+    },
+    {
+      keyCode: 69,
+      keyTrigger: "E",
+      id: "Chord-3",
+      url: "https://s3.amazonaws.com/freecodecamp/drums/Chord_3.mp3",
+    },
+    {
+      keyCode: 65,
+      keyTrigger: "A",
+      id: "Shaker",
+      url: "https://s3.amazonaws.com/freecodecamp/drums/Give_us_a_light.mp3",
+    },
+    {
+      keyCode: 83,
+      keyTrigger: "S",
+      id: "Open-HH",
+      url: "https://s3.amazonaws.com/freecodecamp/drums/Dry_Ohh.mp3",
+    },
+    {
+      keyCode: 68,
+      keyTrigger: "D",
+      id: "Closed-HH",
+      url: "https://s3.amazonaws.com/freecodecamp/drums/Bld_H1.mp3",
+    },
+    {
+      keyCode: 90,
+      keyTrigger: "Z",
+      id: "Punchy-Kick",
+      url: "https://s3.amazonaws.com/freecodecamp/drums/punchy_kick_1.mp3",
+    },
+    {
+      keyCode: 88,
+      keyTrigger: "X",
+      id: "Side-Stick",
+      url: "https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3",
+    },
+    {
+      keyCode: 67,
+      keyTrigger: "C",
+      id: "Snare",
+      url: "https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3",
+    },
+  ];
 
   return (
     <div id="drum-machine">
       <div className="drum-pads">
-        {keyboard.map((letter, idx) => (
-          <Drumpad letter={letter} key={idx} />
+        {heaterKit.map((sample, idx) => (
+          <Drumpad
+            sample={sample}
+            letter={sample.keyTrigger}
+            audio={sample.url}
+            key={idx}
+          />
         ))}
       </div>
 
@@ -21,7 +137,10 @@ const DrumMachine = (props) => {
         <button>
           <i className="fas fa-minus"></i>
         </button>
-        <div className="screen">KIT ONE</div>
+        <div className="screen">
+          <p id="kit-name">KIT NAME</p>
+          <p id="pad-name">PAD NAME</p>
+        </div>
         <button>
           <i className="fas fa-plus"></i>
         </button>
--- src/components/drum-pad.js
+++ src/components/drum-pad.js
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 const Drumpad = (props) => {
+  const playSound = () => {
+    const sound = document.getElementById(props.sample.keyTrigger);
+    sound.currentTime = 0;
+    sound.play();
+  };
+
+  useEffect(() => {
+    document.addEventListener("keydown", (e) => {
+      if (e.key.toUpperCase() === props.letter) {
+        playSound();
+      }
+    });
+  });
+
   return (
-    <div className="drum-pad">
+    <div className="drum-pad" onClick={playSound}>
       <p>{props.letter}</p>
       <div className="light-indicator"></div>
+      <audio className="clip" id={props.letter} src={props.audio} />
     </div>
   );
 };
